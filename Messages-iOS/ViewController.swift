bd43e847d3b6dd594465b217e0780a93286c3177
Michael Penberthy <michaelpenberthy@Michaels-MacBook-Pro.local>
2016-08-23 00:53:45 -0400

    Initial commit

---
+++ Messages-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messages-iOS
+//
+//  Created by Michael Penberthy on 8/23/16.
+//  Copyright © 2016 Michael Penberthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
